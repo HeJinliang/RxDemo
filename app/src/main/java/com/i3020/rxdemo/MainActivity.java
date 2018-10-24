49ea4224dbf2052d7381e60983ec8bfe3f273d38
HeJinliang <jinliang8052@163.com>
2018-10-24 15:22:37 +0800

    Initial commit

---
+++ app/src/main/java/com/i3020/rxdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.i3020.rxdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * describe:
+ * Company: 杭州洞见科技有限公司（www.i3020.com）
+ * Created by HeJinLiang on 2018-10-24 15:07:28
+ */
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+
+}
